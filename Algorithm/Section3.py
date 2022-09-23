21e2464259164086d139c1bef6729797f8f918b4
Davin <davinmailsklh@gmail.com>
2022-09-23 11:33:16 +0700

    Algorithm

--- Algorithm/Section3.py
+++ Algorithm/Section3.py
@@ -1,15 +1,14 @@
-def combination(t, l, nums):      
+def combination(t, l):    
+    nums = [1,2,3,4,5,6,7,8,9]  
     res = []
     if l == 1 and t <= 9:
         res.append([t])
     elif l == 0:
         return res
     else:
-        for i in nums:            
-            nums = [1,2,3,4,5,6,7,8,9]
-            if i <= int(t / l):
-                nums.pop(0)
-                array = combination(t - i, l - 1, nums)
+        for i in nums:                    
+            if i <= int(t / l):                
+                array = combination(t - i, l - 1)
                 for n in array:
                     currentArr = n
                     if i not in currentArr:
@@ -24,6 +23,5 @@ def combination(t, l, nums):
 
 if __name__ == "__main__":
     length = input("Input length of combination : ")
-    total = input("Input total of combination: ")
-    nums = [1,2,3,4,5,6,7,8,9]
-    print(combination(int(total), int(length), nums))
+    total = input("Input total of combination: ")    
+    print(combination(int(total), int(length)))
