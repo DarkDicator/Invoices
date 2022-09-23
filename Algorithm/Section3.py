be51eac991b3166c7ac373f7255ab945a061c915
Davin <davinmailsklh@gmail.com>
2022-09-23 11:27:50 +0700

    Algorithm commit

--- Algorithm/Section3.py
+++ Algorithm/Section3.py
@@ -1,18 +1,22 @@
-def combination(t, l, min):      
+def combination(t, l, nums):      
     res = []
     if l == 1 and t <= 9:
         res.append([t])
     elif l == 0:
         return res
     else:
-        for i in range(min, 9, 1):
+        for i in nums:            
+            nums = [1,2,3,4,5,6,7,8,9]
             if i <= int(t / l):
-                array = combination(t - i, l - 1, min + 1)
-                for n in range(0, len(array), 1):
-                    currentArr = array[n]
+                nums.pop(0)
+                array = combination(t - i, l - 1, nums)
+                for n in array:
+                    currentArr = n
                     if i not in currentArr:
                         currentArr.insert(0, i)
-                        res.append(currentArr)            
+                        currentArr.sort()
+                        if currentArr not in res:                            
+                            res.append(currentArr)            
     return res
 
 
@@ -21,4 +25,5 @@ def combination(t, l, min):
 if __name__ == "__main__":
     length = input("Input length of combination : ")
     total = input("Input total of combination: ")
-    print(combination(int(total), int(length), 1))
+    nums = [1,2,3,4,5,6,7,8,9]
+    print(combination(int(total), int(length), nums))
